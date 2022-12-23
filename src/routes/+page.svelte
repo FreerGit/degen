ce19d4a732b063d533de78fe05c99683cb5855d0
Fredrik Eriksson <Freer320@student.liu.se>
2022-12-23 04:15:29 +0100

    websocket

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	const ENDPOINT = 'wss://stream.bybit.com/realtime_public';
+
+	onMount(() => {
+		const ws = new WebSocket(ENDPOINT);
+		ws.onopen = () => {
+			ws.send('{"op": "subscribe", "args": ["orderBookL2_25.BTCUSDT"]}');
+		};
+		ws.onmessage = (message) => {};
+	});
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
