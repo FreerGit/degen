ed9057e7fb9e07b03ecf0f1e36107a673bd419a1
Sven <fredrik-98@live.se>
2023-07-04 18:08:55 +0200

    fix: bybit svg instead of png

--- src/lib/components/exchange_logo.svelte
+++ src/lib/components/exchange_logo.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Binance from '$lib/assets/binance.svelte';
 	import type { Exchange } from '$lib/types';
+	import Bybit from '../assets/bybit.svelte';
 
 	export let exchange: Exchange;
 </script>
@@ -8,5 +9,6 @@
 {#if exchange == 'Binance'}
 	<Binance />
 {:else if exchange == 'Bybit'}
-	<img width="20px" height="20px" src="src/lib/assets/bybit.png" alt={exchange}/>
+	<Bybit/>
+<!-- <img width="20px" height="20px" src="src/lib/assets/bybit.png" alt={exchange}/> -->
 {/if}
